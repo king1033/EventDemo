30f3108f2a51a7a16b5a51affa47373973c579cd
King1033 <1786551559@qq.com>
2016-08-31 17:10:43 +0800

    The ListView downward slide in vertical direction and the transverse sliding in the event of each ITEM by event distribution mechanism to solve conflict

--- app/src/main/java/com/king1033/eventdemo/CustomListView.java
+++ app/src/main/java/com/king1033/eventdemo/CustomListView.java
@@ -1,18 +1,28 @@
 package com.king1033.eventdemo;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
-import java.util.List;
 
 /**
  * @Author:king1033
  * @Desc:
  * @Time:2016/8/31
  */
-public class CustomListView extends ListView{
+public class CustomListView extends ListView {
+
+    private static final String TAG = "king1033";
+    private int startX, startY;
+    private View childView;
+    private int distanceX;
+    private boolean isLocked;
+
     public CustomListView(Context context) {
         super(context);
     }
@@ -27,10 +37,39 @@ public class CustomListView extends ListView{
 
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
-       switch(ev.getAction()){
-
-       }
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_MOVE:
+                distanceX = (int) (startX - ev.getX());
+                Log.i(TAG, "onTouchEvent: " + distanceX);
+                if (distanceX > -300 && distanceX < -50) {
+                    childView.scrollTo(distanceX, 0);
+                    isLocked = true;
+                }
+                break;
+            case MotionEvent.ACTION_DOWN:
+                if (childView != null) {
+                    childView.scrollTo(0, 0);
+                    isLocked = false;
+                }
+                startX = (int) ev.getX();
+                startY = (int) ev.getY();
+                //通过X、Y坐标获取ListView的Position
+                int position = pointToPosition(startX, startY);
+                //判断Postion是否有效
+                if (position == AdapterView.INVALID_POSITION) {
+                    return super.onTouchEvent(ev);
+                }
+                childView = getChildAt(position - getFirstVisiblePosition());
+                childView.setBackgroundColor(Color.BLUE);
+                break;
+            case MotionEvent.ACTION_UP:
+                childView.setBackgroundColor(Color.TRANSPARENT);
+                break;
+        }
+        if (isLocked) {
+            return true;
+        }
         return super.onTouchEvent(ev);
-
     }
 }
+
--- app/src/main/java/com/king1033/eventdemo/MainActivity.java
+++ app/src/main/java/com/king1033/eventdemo/MainActivity.java
@@ -2,15 +2,51 @@ package com.king1033.eventdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 
 /**
  * 实现ListView的item的侧滑效果
  */
 public class MainActivity extends AppCompatActivity {
 
+    private CustomListView mListView;
+    private MyAapter myAapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mListView = (CustomListView) findViewById(R.id.custom_list_view);
+        myAapter = new MyAapter();
+        mListView.setAdapter(myAapter);
+    }
+
+    private class MyAapter extends BaseAdapter{
+        @Override
+        public int getCount() {
+            return 20;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = convertView;
+            if(view == null){
+                view = LayoutInflater.from(MainActivity.this).inflate(R.layout.item_view,parent,false);
+            }
+            return view;
+        }
     }
 }
